61c6d4a19628784f9b262dd0105fcaa3aaf112f3
Raul Garcia <punkvomit21@gmail.com>
2021-04-07 17:16:39 -0400

    created heading, button and userpoints components

--- src/UserPoints.js
+++ src/UserPoints.js
@@ -1,48 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function UserPoints() {
-
-  const [username, setUsername] = useState(``);
-  const [isUsernameSaved, setIsUsernameSaved] = useState(false);
-  const [points, setPoints] = useState(0);
-
-
+function Heading({ username, points }) {
   return (
-    <>
-      {isUsernameSaved ? (
-        <>
-          <h1>
-            {username} : {points}
-          </h1>
-          <p>
-            <button onClick={() => setPoints(points + 1)}>Add Point</button>
-            <button onClick={() => setPoints(0)}>Clear Points</button>
-          </p>
-          <p>
-            <button
-              onClick={() => {
-                setIsUsernameSaved(false);
-              }}>Edit Username</button>
-            <button
-              onClick={() => {
-                setUsername(``);
-                setPoints();
-                setIsUsernameSaved(false);
-              }}>Clear Username</button>
-          </p>
-        </>
-      ) : (
-        <p>
-          <input
-            id="username"
-            onChange={e => setUsername(e.target.value)} placeholder="Username" />
-          <button
-            onClick={() => setIsUsernameSaved(true)}>Save Username</button>
-        </p>
-      )}
-    </>
+    <h1>
+      {username} : {points}
+    </h1>
   );
 }
+
+function Button({ label, onClick }) {
+  return <button onClick={() => onClick()}>{label}</button>
+}
+
+function UserPoints() {
+  const [username, setUsername] = useState(null);
+  const [isUsernameSaved, setIsusernameSaved] = useState(false);
+  const [points, setPoints] = useState(0);
+
+  return (
+  
+  )
+}
+
 
 export default UserPoints;
