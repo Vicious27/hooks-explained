09b9fd6d01fa4b9c4b17bbdf6836434ae1610d17
Raul Garcia <punkvomit21@gmail.com>
2021-04-14 16:17:01 -0400

    set up workspace for exercises with hooks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <header className="App-header">
-        <h1 className="title">React Hooks explained</h1>
+        <h1 className="title">React Hooks exercise</h1>
       </header>
       <Appsus />
     </>
