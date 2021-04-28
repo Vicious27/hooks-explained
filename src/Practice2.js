c5bc7d7c631ecc195f871fa2403b9443b3fe2e7d
Raul Garcia <punkvomit21@gmail.com>
2021-04-28 17:48:55 -0400

    added commented instructions to practice2 component

--- src/Practice2.js
+++ src/Practice2.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 
 const Practice2 = () => {
 
+  /*
+   * 1. Create homePoints and awayPoints in state using hooks
+   * 2. Display the points for the home and away teams
+   * 3. Add a button for each team to add one point
+   * 4. Add an onClick event to the Reset button that sets both points to zero
+   */
+
   return (
     <>
       <h1>Scoreboard</h1>
