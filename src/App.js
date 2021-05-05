f09dc3cfcaee1c52387896b3893f1d96cef50ec6
Raul Garcia <punkvomit21@gmail.com>
2021-05-05 14:18:08 -0400

    created practice 3 import n exported as necessary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import Counter from "./Practice1";
 // import Practice2 from "./Practice2";
+import Practice3 from "./Practice3";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </header>
       {/* <Counter /> */}
       {/* <Practice2 /> */}
+      <Practice3 />
     </>
   )
 };
