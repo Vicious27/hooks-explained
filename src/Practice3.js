f8c152181b87db835af96a1447a705738ecd675a
Raul Garcia <punkvomit21@gmail.com>
2021-05-05 21:42:17 -0400

    use useEffect to make a fetch call

--- src/Practice3.js
+++ src/Practice3.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const Practice3 = () => {
-  /*
-   * 1. Create a value in state using hooks called catImg
-   * 2. Use useEffect to make an API call to https://aws.random.cat/meow for a random picture of a cat
-   * 3. Add the url for the image to the page below the title
-   * 4. Pass an empty array of dependencies to useEffect so it only fetches on initial page load
-   */
+
+  const [catImg, setCatImg] = useState(``);
+
+  useEffect(() => {
+    fetch(`https://source.unsplash.com/random`)
+      .then((response) => response.json)
+      .then((data) => setCatImg(data.file))
+      .catch((error) => console.error(error))
+  }, []);
+
+
 
   return (
     <>
-      <h1>Random Cat Photo</h1>
-      <img src="" alt="Random Cat Image" width="500" />
+      <h1>Random Photo</h1>
+      {/* eslint-disable-next-line */}
+      <img src={catImg} alt="Random Cat Image" width="500" />
     </>
   );
 };
 
 export default Practice3;
+
+/*
+ * 1. Create a value in state using hooks called catImg
+ * 2. Use useEffect to make an API call to https://aws.random.cat/meow for a random picture of a cat
+ * 3. Add the url for the image to the page below the title
+ * 4. Pass an empty array of dependencies to useEffect so it only fetches on initial page load
+ */
